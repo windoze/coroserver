3eef8b1790b9f4beade3ac9e63809889e1af8147
Windoze <windoze.net@gmail.com>
2013-02-25 10:19:23 +0800

    Initial Commit

---
+++ coroserver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  coroserver
+//
+//  Created by Xu Chen on 13-2-25.
+//  Copyright (c) 2013年 Xu Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
